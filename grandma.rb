b772e4ae6909f41aee92404619ed5bba172eeced
Shane Geary <stg4462@comcast.net>
2020-01-20 17:02:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,8 @@
 def speak_to_grandma(string)
   if string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif string.upcase
+  else string.lowercase
     return "NO, NOT SINCE 1938!"
+  elsif string.upcase "I LOVE YOU TOO PUMPKIN!"
   end
 end
