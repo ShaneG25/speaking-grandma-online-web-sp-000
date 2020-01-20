355be9dd671c1c7ceac0376f3ac19269a191c8ea
Shane Geary <stg4462@comcast.net>
2020-01-20 16:58:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,6 +14,7 @@
 def speak_to_grandma(string)
   if string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
-    elsif string.upcase "NO, NOT SINCE 1938!"
+  elsif string.upcase
+    return "NO, NOT SINCE 1938!"
   end
 end
